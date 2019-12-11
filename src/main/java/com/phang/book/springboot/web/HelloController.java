9cbcc92c6e8affc426df8eaee10ac77749039d6e
황지혜 <jh.hwang@crscube.co.kr>
2019-12-11 14:59:47 +0900

    HelloController에서 HelloResponseDto를 사용하도록 코드를 추가

--- src/main/java/com/phang/book/springboot/web/HelloController.java
+++ src/main/java/com/phang/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.phang.book.springboot.web;
 
+import com.phang.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -19,5 +21,19 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+    /*
+    @RequestParam
+        - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+        - 여기서는 외부에서 (@RequestParam("name"))이란 이름으로 넘긴 파라미터를
+        메서드 파라미터 name(String name)에 저장하게 된다.
+     */
+                                    @RequestParam("amount") int amount) {
+        // name과 amount는 API를 호출하는 곳에서 넘겨준 값들이다.
+        return new HelloResponseDto(name, amount);
     }
 }
