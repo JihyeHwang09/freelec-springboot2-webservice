d38037168533d821f02e112a69eb81fc27519b0e
황지혜 <jh.hwang@crscube.co.kr>
2019-12-16 10:49:28 +0900

    게시글 수정 화면 만들기

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,14 +4,15 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
-
+        /*
+        $('#btn-update').on('click')
+        : btn-update라는 idㄹㄹ 가진 HTML 엘리먼트에 click 이벤트가 발생할 때, update function을
+        실행하도록 이벤트를 등록한다.
+         */
         $('#btn-update').on('click', function () {
             _this.update();
         });
 
-        $('#btn-delete').on('click', function () {
-            _this.delete();
-        });
     },
     save : function () {
         var data = {
@@ -33,6 +34,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
+    // update : function (): 신규로 추가될 update function
     update : function () {
         var data = {
             title: $('#title').val(),
@@ -42,8 +44,20 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
+                   /*
+                   type: 'PUT'
+                    - 여러 HTTP Method 중 PUT 메소드를 선택한다.
+                    - PostsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에
+                    PUT을 사용해야 한다.
+                    - REST에서 HTTP Method 매핑
+                        - 생성(Create): POST
+                        - 읽기(Read): GET
+                        - 수정(Update): PUT
+                        - 삭제(Delete): DELETE
+                    */
                    type: 'PUT',
-                   url: '/api/v1/posts/'+id,
+                    // 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다.
+                   url: '/api/v1/posts/'+ id,
                    dataType: 'json',
                    contentType:'application/json; charset=utf-8',
                    data: JSON.stringify(data)
@@ -54,22 +68,6 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
-    delete : function () {
-        var id = $('#id').val();
-
-        $.ajax({
-                   type: 'DELETE',
-                   url: '/api/v1/posts/'+id,
-                   dataType: 'json',
-                   contentType:'application/json; charset=utf-8'
-               }).done(function() {
-            alert('글이 삭제되었습니다.');
-            //  window.location.href = '/': 글 등록이 성공하면, 메인페이지(/)로 이동한다.
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    }
 
 };
 
